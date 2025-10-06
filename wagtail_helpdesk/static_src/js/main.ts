eb94686328b5c48b3b6b6dbaa77f578f6091bbc5
Eline-Koch <113468440+Eline-Koch@users.noreply.github.com>
2025-10-06 15:26:00 +0200

    add relative CO2 emmissions calc, partial images, text

--- wagtail_helpdesk/static_src/js/main.ts
+++ wagtail_helpdesk/static_src/js/main.ts
@@ -2,69 +2,9 @@ import { Application } from "@hotwired/stimulus";
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
 import Konva from "konva";
 
-console.log("test");
-
 window.Stimulus = Application.start();
 const context = require.context("./controllers", true, /\.js$/);
 Stimulus.load(definitionsFromContext(context));
-
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-const img = new Image();
-img.src =
-  "https://www.gardengourmet.nl/sites/default/files/styles/768_width/public/2024-08/vegan-burger.png?itok=pXKWu9wB";
-
-canvas.width = 800;
-canvas.height = 600;
-
-let canvasCirclePositions = [
-  [0.5, 0.5],
-  [0.5, 0.2],
-  [0.71, 0.29],
-  [0.8, 0.5],
-  [0.71, 0.71],
-  [0.5, 0.8],
-  [0.29, 0.71],
-  [0.2, 0.5],
-  [0.29, 0.29],
-];
-
-let circleRadius = 0.1 * canvas.height;
-
-let amounts = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-ctx.font = "20px Arial";
-ctx.fillText("Carbon Calculator", canvas.width / 20, canvas.height / 20);
-
-ctx.beginPath();
-for (let [i, position] of canvasCirclePositions.entries()) {
-  ctx.moveTo(
-    position[0] * canvas.width + circleRadius,
-    position[1] * canvas.height
-  );
-  ctx.arc(
-    position[0] * canvas.width,
-    position[1] * canvas.height,
-    circleRadius,
-    0,
-    2 * Math.PI
-  );
-
-  for (let j = 0; j < amounts[i]; j++)
-    img.addEventListener("load", () => {
-      ctx.drawImage(
-        img,
-        position[0] * canvas.width -
-          0.5 * circleRadius +
-          j * (circleRadius / amounts[i]),
-        position[1] * canvas.height - (0.5 * circleRadius) / amounts[i],
-        circleRadius / amounts[i],
-        circleRadius / amounts[i]
-      );
-    });
-}
-ctx.stroke();
 
 // first we need to create a stage
 const stage = new Konva.Stage({
@@ -80,13 +20,26 @@ const layer = new Konva.Layer();
 let circlePositions = [
   [0.5, 0.5],
   [0.5, 0.2],
-  [0.71, 0.29],
+  [0.69, 0.28],
   [0.8, 0.5],
-  [0.71, 0.71],
+  [0.69, 0.72],
   [0.5, 0.8],
-  [0.29, 0.71],
+  [0.31, 0.72],
   [0.2, 0.5],
-  [0.29, 0.29],
+  [0.31, 0.28],
+];
+let circleRadius = stage.width() * 0.09;
+let amounts = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const co2values = [
+  ["cheeseburger", 3],
+  ["broodje kroket", 1.34],
+  ["broodje gezond", 1.29],
+  ["boterham kaas", 0.57],
+  ["vegaburger", 0.4],
+  ["test1", 20],
+  ["test2", 10],
+  ["test3", 1.6],
+  ["test4", 0.1]
 ];
 
 function createCircle(x, y, radius) {
@@ -96,6 +49,19 @@ function createCircle(x, y, radius) {
     radius: radius,
     fill: "white",
     stroke: "black",
+  });
+}
+
+function createText(x, y, text, fontSize, fontFamily, fill) {
+  return new Konva.Text({
+    x: x - stage.width() / 2,
+    y: y,
+    text: text,
+    fontSize: fontSize,
+    fontFamily: fontFamily,
+    fill: fill,
+    width: stage.width(),
+    align: "center"
   });
 }
 
@@ -112,30 +78,100 @@ function createImage(x, y, width, height, source) {
   });
 }
 
+layer.add(
+  createText(
+    stage.width() / 2,
+    0,
+    "1 Cheeseburger geeft evenveel CO2-uitstoot als:",
+    30,
+    "Arial",
+    "black"
+  )
+);
+
 for (let [i, position] of circlePositions.entries()) {
   // add the shapes to the layer
   layer.add(
     createCircle(
       stage.width() * position[0],
       stage.height() * position[1],
-      stage.width() * 0.08
+      circleRadius
     )
   );
 
-  for (let j = 0; j < amounts[i]; j++) {
-    layer.add(
-      createImage(
-        position[0] * stage.width() -
-          0.5 * circleRadius +
-          j * (circleRadius / amounts[i]),
-        position[1] * stage.height() - (0.5 * circleRadius) / amounts[i],
-        circleRadius / amounts[i],
-        circleRadius / amounts[i],
-        "https://www.gardengourmet.nl/sites/default/files/styles/768_width/public/2024-08/vegan-burger.png?itok=pXKWu9wB"
-      )
-    );
+  layer.add(
+    createText(
+      stage.width() * position[0],
+      stage.height() * position[1] - stage.height() / 12,
+      co2values[i][0],
+      16,
+      "Calibri",
+      "green"
+    )
+  );
+
+  layer.add(
+    createText(
+      stage.width() * position[0],
+      stage.height() * position[1] + stage.height() / 14,
+      Math.round((co2values[0][1]  / co2values[i][1]) * 100) / 100,
+      14,
+      "Calibri",
+      "black"
+    )
+  );
+
+  let amountCeil = Math.ceil(co2values[0][1] / co2values[i][1]);
+  let lastAmount = ( co2values[0][1] / co2values[i][1]) % 1;
+  if (lastAmount == 0) {
+    lastAmount = 1
+  } 
+
+  console.log(lastAmount);
+
+  for (let j = 0; j < amountCeil; j++) {
+    if(j == amountCeil - 1) {
+      const imageObj = new Image();
+      imageObj.src = "https://www.gardengourmet.nl/sites/default/files/styles/768_width/public/2024-08/vegan-burger.png?itok=pXKWu9wB";
+      imageObj.onload = () => {
+        const halfWidth = imageObj.width * lastAmount;
+
+        const konvaImage = new Konva.Image({
+          x: position[0] * stage.width() -
+            0.5 * circleRadius +
+            j * (circleRadius / amountCeil),
+          y: position[1] * stage.height() - (0.5 * circleRadius) / amountCeil,
+          image: imageObj,
+          // crop defines what part of the image is visible
+          crop: {
+            x: 0,
+            y: 0,
+            width: halfWidth,
+            height: imageObj.height,
+          },
+          width: (circleRadius / amountCeil) * lastAmount,
+          height: circleRadius / amountCeil
+        });
+
+        layer.add(konvaImage);
+      };
+    }
+
+    else {
+      layer.add(
+        createImage(
+          position[0] * stage.width() -
+            0.5 * circleRadius +
+            j * (circleRadius / amountCeil),
+          position[1] * stage.height() - (0.5 * circleRadius) / amountCeil,
+          circleRadius / amountCeil,
+          circleRadius / amountCeil,
+          "https://www.gardengourmet.nl/sites/default/files/styles/768_width/public/2024-08/vegan-burger.png?itok=pXKWu9wB"
+        )
+      );
+    }
   }
-}
+} 
 
 // add the layer to the stage
 stage.add(layer);
