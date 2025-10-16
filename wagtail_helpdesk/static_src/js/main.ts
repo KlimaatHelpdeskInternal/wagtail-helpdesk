84afb870256f6dc736b086cfccc5dca2f46b7bc4
ElineKoch <eline.koch@hotmail.com>
2025-10-16 12:28:46 +0200

    co2category data in seperate .ts file

--- wagtail_helpdesk/static_src/js/main.ts
+++ wagtail_helpdesk/static_src/js/main.ts
@@ -1,59 +1,14 @@
 import { Application } from "@hotwired/stimulus";
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
 import Konva from "konva";
+import { co2categories } from "./co2categories.ts";
 
 window.Stimulus = Application.start();
 const context = require.context("./controllers", true, /\.js$/);
 Stimulus.load(definitionsFromContext(context));
 
 let circleCount = 9;
-const categories = [
-  {
-    name: "kg CO2",
-    value: 1,
-    icon: "https://cdn-icons-png.flaticon.com/512/3492/3492640.png",
-  },
-  {
-    name: "cheeseburgers",
-    value: 3,
-    icon: "https://cdn-icons-png.flaticon.com/512/3075/3075977.png",
-  },
-  {
-    name: "vegaburgers",
-    value: 0.4,
-    icon: "https://cdn-icons-png.flaticon.com/512/1687/1687009.png",
-  },
-  {
-    name: "uur autorijden",
-    value: 7,
-    icon: "https://cdn-icons-png.flaticon.com/512/8308/8308414.png",
-  },
-  {
-    name: "uur vliegen",
-    value: 400,
-    icon: "https://cdn-icons-png.flaticon.com/512/7893/7893979.png",
-  },
-  {
-    name: "keer 10 min. douchen",
-    value: 0.6,
-    icon: "https://cdn-icons-png.flaticon.com/512/760/760637.png",
-  },
-  {
-    name: "dagen computer gebruiken",
-    value: 0.48,
-    icon: "https://img.freepik.com/premium-vector/desktop-computer-icon-vector-design-template-simple-clean_1309366-1987.jpg",
-  },
-  {
-    name: "dagen huis verwarmen (gas)",
-    value: 5.22,
-    icon: "https://cdn-icons-png.flaticon.com/512/9879/9879880.png",
-  },
-  {
-    name: "dagen huis verwarmen (warmtepomp)",
-    value: 3.13,
-    icon: "https://us.123rf.com/450wm/vectorwin/vectorwin2401/vectorwin240100503/221481784-heat-recovery-system-geothermal-color-icon-vector-illustration.jpg?ver=6",
-  },
-];
+let categories = co2categories;
 let currentCategories = categories.slice();
 
 const select = document.getElementById("co2categories");
