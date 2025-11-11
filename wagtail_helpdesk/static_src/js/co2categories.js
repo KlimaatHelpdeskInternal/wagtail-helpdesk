d070f7f2d3c11c06c5fb94407c241550106bedf7
ChrisVrolijkKH <chris@vrolijk.com>
2025-11-11 16:06:52 +0100

    move CO2 code to function

--- wagtail_helpdesk/static_src/js/co2categories.js
+++ wagtail_helpdesk/static_src/js/co2categories.js
@@ -1,6 +1,10 @@
-const rawtext = carbonemissioncategoriesvar;
-export const co2categories = ReadCO2CategoriesFromJSONFormat(rawtext);
-
+export const co2categories=null;
+function LoadCarbonCategories()
+{
+  alert('In loadCarboncategories');
+  const rawtext = carbonemissioncategoriesvar;
+  co2categories = ReadCO2CategoriesFromJSONFormat(rawtext);
+}
 function ReadCategoryNamesFromConfig(co2categories) {
   var categorynames = [];
 
@@ -33,51 +37,3 @@ function ReadCarbonCalculatorConfig(json_data) {
   return co2categories;
 
 }
-/*
-export const co2categories = [
-  {
-    name: "kg CO2",
-    value: 1,
-    icon: "https://cdn-icons-png.flaticon.com/512/3492/3492640.png",
-  },
-  {
-    name: "cheeseburgers",
-    value: 3,
-    icon: "https://cdn-icons-png.flaticon.com/512/3075/3075977.png",
-  },
-  {
-    name: "vegaburgers",
-    value: 0.4,
-    icon: "https://cdn-icons-png.flaticon.com/512/1687/1687009.png",
-  },
-  {
-    name: "uur autorijden",
-    value: 7,
-    icon: "https://cdn-icons-png.flaticon.com/512/8308/8308414.png",
-  },
-  {
-    name: "uur vliegen",
-    value: 400,
-    icon: "https://cdn-icons-png.flaticon.com/512/7893/7893979.png",
-  },
-  {
-    name: "keer 10 min. douchen",
-    value: 0.6,
-    icon: "https://cdn-icons-png.flaticon.com/512/760/760637.png",
-  },
-  {
-    name: "dagen computer gebruiken",
-    value: 0.48,
-    icon: "https://img.freepik.com/premium-vector/desktop-computer-icon-vector-design-template-simple-clean_1309366-1987.jpg",
-  },
-  {
-    name: "dagen huis verwarmen (gas)",
-    value: 5.22,
-    icon: "https://cdn-icons-png.flaticon.com/512/9879/9879880.png",
-  },
-  {
-    name: "dagen huis verwarmen (warmtepomp)",
-    value: 3.13,
-    icon: "https://us.123rf.com/450wm/vectorwin/vectorwin2401/vectorwin240100503/221481784-heat-recovery-system-geothermal-color-icon-vector-illustration.jpg?ver=6",
-  },
-];*/
