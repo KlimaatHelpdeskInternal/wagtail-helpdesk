e5f975e171eee28b84769be91bd3716fd8509c86
ChrisVrolijkKH <chris@vrolijk.com>
2026-02-26 12:02:50 +0100

    Copy video in answer to video 2 (#38) (#39)
    * daily checkin
    
    * daily commit
    
    * daily commit
    
    * commit
    
    * Button to switch between video and text
    
    * One button to switch between video and text
    
    * When video is visible, page_content is hidden and the other way around.
    
    * Commented out tags
    
    * page_content is always visible if there isn't a video
    
    * Changed 'button' and added hover. Button centers when viewing on phone,
    
    * Added reading time
    
    * Removed toggle-link: focus
    
    * Reading time is always shown with or without video
    
    * page_content is always collapsed
    
    * Added "Read less" if there isn't a video on the page
    
    * Button and video are centered
    
    ---------
    
    Co-authored-by: Chris Vrolijk <Chris.Vrolijk@klimaathelpdesk.org>
    Co-authored-by: shiyibutter <shiyibutter@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { watch } = require("fs");
 
 const source = path.resolve(path.join("./wagtail_helpdesk", "./static_src"));
 const destination = path.resolve(path.join("./wagtail_helpdesk", "./static", "./wagtail_helpdesk"));
@@ -19,6 +20,11 @@ module.exports = (env, argv) => {
       path: destination,
       filename: "[name].js",
       clean: true,
+    },
+    watch: true,
+    watchOptions: {
+      aggregateTimeout: 200,
+      poll: 1000,
     },
     devtool: isProductionMode ? false : "inline-source-map",
     module: {
