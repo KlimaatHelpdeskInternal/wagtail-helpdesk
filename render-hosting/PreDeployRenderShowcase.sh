03dcb51dd3602ac6f7db30d3fcccdfc7505c5915
ChrisVrolijkKH <chris@vrolijk.com>
2026-05-01 14:30:19 +0200

    add showcase settings

--- render-hosting/PreDeployRenderShowcase.sh
+++ render-hosting/PreDeployRenderShowcase.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
-python manage.py collectstatic --no-input --settings=settings.render-test
+python manage.py collectstatic --no-input --settings=settings.render-showcase
 # Apply any outstanding database migrations
-python manage.py makemigrations --settings=settings.render-test
-python manage.py migrate --settings=settings.render-test
+python manage.py makemigrations --settings=settings.render-showcase
+python manage.py migrate --settings=settings.render-showcase
